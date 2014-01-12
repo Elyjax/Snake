76aa971b7abe9547951557e07a9e285bfaefd20b
Elyjax <arthur.burgada@gmail.com>
2014-01-12 12:21:56 +0100

    Simplifies the management of control evenement
    Now the new direction is set by a dictionnary
    Escape key now close the application

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+# Importe la biblioteque pygame
 import pygame
-from pygame.locals import *
+# La syntaxe from x import y permet de ne pas devoir ecrire x.y mais juste y
+# L'etoile indique que l'on importe tout
+from pygame.locals import *  # Quelques constantes utiles (K_UP, K_RIGHT...)
 from Serpent import *
 from constantes import *
 
@@ -10,6 +13,13 @@ fenetre = pygame.display.set_mode((tailleCase * nombreCasesLargeur,
                                    tailleCase * nombreCasesHauteur))
 # Creation de l'objet de type Serpent
 serpent = Serpent(0, 0)
+# Cree une table de correspondance entre les constantes qui represente les
+# fleche directionnelles et le nom de la direction
+table = dict()
+table[K_UP] = "haut"
+table[K_RIGHT] = "droite"
+table[K_DOWN] = "bas"
+table[K_LEFT] = "gauche"
 compteur = 0
 clock = pygame.time.Clock()
 ouvert = True
@@ -18,23 +28,18 @@ while ouvert:
         # Ferme l'application quand on clique sur la croix
         if event.type == QUIT:
             ouvert = False
-        # Change la direction du serpent lors de l'appui sur une touche fleche
         if event.type == KEYDOWN:
-            if event.key == K_UP:
-                serpent.changerDirection("haut")
-            if event.key == K_RIGHT:
-                serpent.changerDirection("droite")
-            if event.key == K_DOWN:
-                serpent.changerDirection("bas")
-            if event.key == K_LEFT:
-                serpent.changerDirection("gauche")
-            # L'appui sur une touche lance automatique une mise a jour
-            # afin d'eviter que certaine commande ne soit pas prise en
-            # compte car une autre touche a ete pressee avant le cycle
-            # de mise a jour
-            serpent.miseAJour()
-            temps = 0
-    # On ajoute le temps ecoule depuis la derniere misea jour a un compteur
+            # Ferme aussi l'application quand on appui sur ESC
+            if event.key == K_ESCAPE:
+                ouvert = False
+            if event.key in (K_UP, K_RIGHT, K_DOWN, K_LEFT):
+                serpent.changerDirection(table[event.key])
+                serpent.miseAJour()
+                compteur = 0
+            # L'appui sur une touche fleche change la direction du serpent et
+            # lance automatique une mise a jour
+
+    # On ajoute le temps ecoule depuis la derniere mise jour a un compteur
     # On met a jour le serpent si compteur > delaisMiseAJour
     # Plus delaisMiseAJour est grand plus le serpent sera lent
     compteur += clock.tick()
