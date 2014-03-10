e23c16acfb5632da72150d8be68affa944b01ddc
arthur <arthur.burgada@gmail.com>
2014-03-10 11:29:26 +0100

    Add a menu at the start
    You can select Play or Quit

--- main.py
+++ main.py
@@ -2,106 +2,49 @@
 import pygame
 # La syntaxe from x import y permet de ne pas devoir ecrire x.y mais juste y
 # L'etoile indique que l'on importe tout
-from pygame.locals import *  # Quelques constantes utiles (K_UP, K_RIGHT...)
-from Serpent import *
-from Fruit import *
 from constantes import *
-from Musique import *
+from jouer import *
 
 pygame.init()
+
 # Ouvre une fenetre de tailleCase * nombreCasesLargeur
 #                  sur tailleCase * nombreCasesHauteur
 fenetre = pygame.display.set_mode((tailleCase * nombreCasesLargeur,
                                    tailleCase * nombreCasesHauteur))
-# Creation de l'objet de type Serpent
-serpent = Serpent(0, 0)
-# Creation de l'objet de type fruits
-fruits = list()
-fruits.append(Fruit(serpent))
-#Creation de l'objet de type Musique
-musique = Musique()
-musique.MoteurMusique()
 
-# Creation police de caractere pour texte potentiel
-pygame.font.init()
-font = pygame.font.SysFont("default", 30)
-
-# Cree une table de correspondance entre les constantes qui represente les
-# fleche directionnelles et le nom de la direction
-table = dict()
-table[K_UP] = "haut"
-table[K_RIGHT] = "droite"
-table[K_DOWN] = "bas"
-table[K_LEFT] = "gauche"
-meilleurScore = 0
-try:
-    fichierScore = open("score.txt", "r")
-    meilleurScore = int(fichierScore.read())
-    fichierScore.close()
-except IOError: # Si le fichier n'exsite pas, on le cree
-    fichierScore = open("score.txt", "w")
-    fichierScore.write("0")
-    fichierScore.close()
-
-scoreActuel = 0
-compteur = 0
-clock = pygame.time.Clock()
+font = pygame.font.SysFont("default", 40)
+selectionActuelle = 1
 ouvert = True
 while ouvert:
-    for event in pygame.event.get(): # Gestion des evenements
-        # Ferme l'application quand on clique sur la croix
-        if event.type == QUIT:
-            ouvert = False
-        if event.type == KEYDOWN:
-            # Ferme aussi l'application quand on appui sur ESC
-            if event.key == K_ESCAPE:
-                ouvert = False
-            # Teste si la touche est une fleche
-            toucheFleche = (K_UP, K_RIGHT, K_DOWN, K_LEFT)
-            if event.key in toucheFleche:
-                # Teste si le changement c'est bien effectue
-                if serpent.changerDirection(table[event.key]):
-                    serpent.miseAJour(fruits)
-                    compteur = 0
-            # L'appui sur une touche fleche change la direction du serpent et
-            # lance automatique une mise a jour
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			ouvert = False
+		if event.type == KEYDOWN:
+			if event.key == K_RETURN:
+				if selectionActuelle == 1:
+					jouer(fenetre)
+				if selectionActuelle == 2:
+					ouvert = False
+			if event.key == K_UP:
+				selectionActuelle -= 1
+				if selectionActuelle < 1:
+					selectionActuelle = 2
+			if event.key == K_DOWN:
+				selectionActuelle += 1
+				if selectionActuelle > 2:
+					selectionActuelle = 1
 
-    # On ajoute le temps ecoule depuis la derniere mise jour a un compteur
-    # On met a jour le serpent si compteur > delaisMiseAJour
-    # Plus delaisMiseAJour est grand plus le serpent sera lent
-    compteur += clock.tick()
-    if compteur >= delaisMiseAJour:
-        serpent.miseAJour(fruits)
-        compteur = 0
+	couleurJouer = (0, 255, 0)  # Couleur verte
+	couleurQuitter = (0, 255, 0)
+	if selectionActuelle == 1:
+		couleurJouer = (255, 0, 0)  # Couleur rouge
+	if selectionActuelle == 2:
+		couleurQuitter = (255, 0, 0)
 
-    # Si le serpent rencontre un bord du niveau ou se rentre dedans, on quitte
-    if serpent.testCollision():
-        break
-    # Affichage
-    fenetre.fill((0, 0, 0))  # On efface l'ecran
-
-    # Affichage des fruits
-    for fruit in fruits:
-        fruit.afficher(fenetre)
-
-    # Affichage des serpents
-    serpent.afficher(fenetre)
-
-    # Mise a jour des scores
-    scoreActuel = len(serpent.positionsCorps) - 1
-    if scoreActuel > meilleurScore:
-        print meilleurScore
-        meilleurScore = scoreActuel
-        fichierScore = open("score.txt", "w")
-        fichierScore.write(str(meilleurScore))
-        fichierScore.close()
-
-    # Affichage des scores
-    fenetre.blit(font.render("Score : " + str(scoreActuel), 1,
-                             (255, 255, 255)), (0, 0))
-    fenetre.blit(font.render("Meilleur score : " + str(meilleurScore), 1,
-                             (255, 255, 255)), (0, 16))
-
-    pygame.display.flip()  # On actualise l'ecran
+	fenetre.fill((0, 0, 0))  # On efface l'ecran
+	# On affiche le menu
+	fenetre.blit(font.render("Jouer", 1, couleurJouer), (100, 50))
+	fenetre.blit(font.render("Quitter", 1, couleurQuitter), (100, 100))
+	pygame.display.flip()
 
 pygame.quit()
