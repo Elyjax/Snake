b08e3f7a52a32d8037c5cd1af9239d9d91b2dd69
Elyjax <elyjax@riseup.net>
2014-03-22 11:48:06 +0000

    Fix score bug

--- jouer.py
+++ jouer.py
@@ -4,104 +4,105 @@ from Fruit import *
 from Musique import *
 
 def jouer(fenetre):
-	# Creation de l'objet de type Serpent
-	serpent = Serpent(0, 0)
-	# Creation de l'objet de type fruits
-	fruits = list()
-	fruits.append(Fruit(serpent))
-	#Creation de l'objet de type Musique
-	musique = Musique()
-	musique.MoteurMusique()
-	# Chargement de l'image de fond
-	fond = pygame.image.load("Images/Fond.png").convert()
-	# Creation police de caractere pour texte potentiel
-	pygame.font.init()
-	font = pygame.font.SysFont("default", 30)
+    # Creation de l'objet de type Serpent
+        serpent = Serpent(0, 0)
+        # Creation de l'objet de type fruits
+        fruits = list()
+        fruits.append(Fruit(serpent))
+        #Creation de l'objet de type Musique
+        musique = Musique()
+        musique.MoteurMusique()
+        # Chargement de l'image de fond
+        fond = pygame.image.load("Images/Fond.png").convert()
+        # Creation police de caractere pour texte potentiel
+        pygame.font.init()
+        font = pygame.font.SysFont("default", 30)
 
-	# Cree une table de correspondance entre les constantes qui represente les
-	# fleche directionnelles et le nom de la direction
-	table = dict()
-	table[K_UP] = "haut"
-	table[K_RIGHT] = "droite"
-	table[K_DOWN] = "bas"
-	table[K_LEFT] = "gauche"
+        # Cree une table de correspondance entre les constantes qui represente les
+        # fleche directionnelles et le nom de la direction
+        table = dict()
+        table[K_UP] = "haut"
+        table[K_RIGHT] = "droite"
+        table[K_DOWN] = "bas"
+        table[K_LEFT] = "gauche"
 
-	try:
-		fichierScore = open("score.txt", "r")
-		meilleurScore = int(fichierScore.read())
-		fichierScore.close()
-	except IOError: # Si le fichier n'exsite pas, on le cree
-		fichierScore = open("score.txt", "w")
-		fichierScore.write("0")
-		fichierScore.close()
+        meilleurScore = 0
 
-	meilleurScore = 0
-	scoreActuel = 0
-	compteur = 0
-	clock = pygame.time.Clock()
-	ouvert = True
+        try:
+            fichierScore = open("score.txt", "r")
+            meilleurScore = int(fichierScore.read())
+            fichierScore.close()
+        except IOError: # Si le fichier n'exsite pas, on le cree
+            fichierScore = open("score.txt", "w")
+            fichierScore.write("0")
+            fichierScore.close()
 
-	while ouvert:
-		for event in pygame.event.get(): # Gestion des evenements
-			# Ferme l'application quand on clique sur la croix
-			if event.type == QUIT:
-                                musique.Stop()
-				ouvert = False
-			if event.type == KEYDOWN:
-				# Ferme aussi l'application quand on appui sur ESC
-				if event.key == K_ESCAPE:
-                                        musique.Stop()
-					ouvert = False
-				# Teste si la touche est une fleche
-				toucheFleche = (K_UP, K_RIGHT, K_DOWN, K_LEFT)
-				if event.key in toucheFleche:
-					# Teste si le changement c'est bien effectue
-					if serpent.changerDirection(table[event.key]):
-						serpent.miseAJour(fruits)
-						compteur = 0
-				# L'appui sur une touche fleche change la direction du serpent et
-				# lance automatique une mise a jour
+        scoreActuel = 0
+        compteur = 0
+        clock = pygame.time.Clock()
+        ouvert = True
 
-		# On ajoute le temps ecoule depuis la derniere mise jour a un compteur
-		# On met a jour le serpent si compteur > delaisMiseAJour
-		# Plus delaisMiseAJour est grand plus le serpent sera lent
-		compteur += clock.tick()
-		if compteur >= delaisMiseAJour:
-			serpent.miseAJour(fruits)
-			compteur = 0
+        while ouvert:
+            for event in pygame.event.get(): # Gestion des evenements
+                # Ferme l'application quand on clique sur la croix
+                if event.type == QUIT:
+                    musique.Stop()
+                    ouvert = False
+                if event.type == KEYDOWN:
+                    # Ferme aussi l'application quand on appui sur ESC
+                        if event.key == K_ESCAPE:
+                            musique.Stop()
+                            ouvert = False
+                        # Teste si la touche est une fleche
+                        toucheFleche = (K_UP, K_RIGHT, K_DOWN, K_LEFT)
+                        if event.key in toucheFleche:
+                            # Teste si le changement c'est bien effectue
+                                if serpent.changerDirection(table[event.key]):
+                                    serpent.miseAJour(fruits)
+                                    compteur = 0
+                        # L'appui sur une touche fleche change la direction du serpent et
+                        # lance automatique une mise a jour
 
-		# Si le serpent rencontre un bord du niveau ou se rentre dedans, on quitte
-		if serpent.testCollision():
-                        musique.Stop()
-			break
-		# On efface l'ecran
-		fenetre.fill((0, 0, 0))
+            # On ajoute le temps ecoule depuis la derniere mise jour a un compteur
+            # On met a jour le serpent si compteur > delaisMiseAJour
+            # Plus delaisMiseAJour est grand plus le serpent sera lent
+            compteur += clock.tick()
+            if compteur >= delaisMiseAJour:
+                serpent.miseAJour(fruits)
+                compteur = 0
 
-		# On affiche le fond
-		for i in range(0, nombreCasesLargeur):
-			for j in range(0, nombreCasesHauteur):
-				fenetre.blit(fond, (i * tailleCase, j * tailleCase))
+            # Si le serpent rencontre un bord du niveau ou se rentre dedans, on quitte
+            if serpent.testCollision():
+                musique.Stop()
+                break
+            # On efface l'ecran
+            fenetre.fill((0, 0, 0))
 
-		# Affichage des fruits
-		for fruit in fruits:
-			fruit.afficher(fenetre)
+            # On affiche le fond
+            for i in range(0, nombreCasesLargeur):
+                for j in range(0, nombreCasesHauteur):
+                    fenetre.blit(fond, (i * tailleCase, j * tailleCase))
 
-		# Affichage des serpents
-		serpent.afficher(fenetre)
+            # Affichage des fruits
+            for fruit in fruits:
+                fruit.afficher(fenetre)
 
-		# Mise a jour des scores
-		scoreActuel = len(serpent.positionsCorps) - 1
-		if scoreActuel > meilleurScore:
-			meilleurScore = scoreActuel
-			fichierScore = open("score.txt", "w")
-			fichierScore.write(str(meilleurScore))
-			fichierScore.close()
+            # Affichage des serpents
+            serpent.afficher(fenetre)
 
-		# Affichage des scores
-		fenetre.blit(font.render("Score : " + str(scoreActuel), 1,
-								 (255, 255, 255)), (0, 0))
-		fenetre.blit(font.render("Meilleur score : " + str(meilleurScore), 1,
-								 (255, 255, 255)), (0, 16))
+            # Mise a jour des scores
+            scoreActuel = len(serpent.positionsCorps) - 1
+            if scoreActuel > meilleurScore:
+                meilleurScore = scoreActuel
+                fichierScore = open("score.txt", "w")
+                fichierScore.write(str(meilleurScore))
+                fichierScore.close()
 
-		# On actualise l'ecran
-		pygame.display.flip()
+            # Affichage des scores
+            fenetre.blit(font.render("Score : " + str(scoreActuel), 1,
+                (255, 255, 255)), (0, 0))
+            fenetre.blit(font.render("Meilleur score : " + str(meilleurScore), 1,
+                (255, 255, 255)), (0, 16))
+
+            # On actualise l'ecran
+            pygame.display.flip()
